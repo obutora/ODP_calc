68e856d3678c1c06fa136af5651bed6bec671922
chan haga <obutora@gmail.com>
2023-02-24 13:15:22 +0900

    add : const color

--- odp_calc_flutter_client/lib/const.dart
+++ odp_calc_flutter_client/lib/const.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
 final log = Logger();
+
+//color--------------------
+const kWhite = Color(0xFFFFFFFF);
+const kBlack = Color(0xFF262B36);
+const kBlue = Color(0xFF0086C2);
+const kGreen = Color(0xFF84C6B5);
+const kOrange = Color(0xFFFF836B);
