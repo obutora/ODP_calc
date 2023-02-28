d3b770ad51dee8f237ac4c587eb7d4692c78229f
chan haga <obutora@gmail.com>
2023-02-28 23:39:34 +0900

    update: inputDecoration

--- odp_calc_flutter_client/lib/widget/style/search_input_decoration.dart
+++ odp_calc_flutter_client/lib/widget/style/search_input_decoration.dart
@@ -8,6 +8,7 @@ InputDecoration searchInputDecoration({required String hintText}) {
     hintText: hintText,
     hintStyle:
         kTextSearchBox.copyWith(color: kGreen, fontWeight: FontWeight.w400),
+    errorStyle: kTextDescription2.copyWith(color: kOrange),
     enabledBorder: OutlineInputBorder(
       borderSide: const BorderSide(color: kGreen, width: 1.5),
       borderRadius: BorderRadius.circular(8),
@@ -15,6 +16,14 @@ InputDecoration searchInputDecoration({required String hintText}) {
     fillColor: kGreen,
     focusedBorder: OutlineInputBorder(
       borderSide: const BorderSide(color: kGreen, width: 3),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: kGreen, width: 3),
+      borderRadius: BorderRadius.circular(8),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: kOrange, width: 3),
       borderRadius: BorderRadius.circular(8),
     ),
   );
