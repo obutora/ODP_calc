3d9ec5c0032db3fe7c677d1d8c7d01b6c3209c96
chan haga <obutora@gmail.com>
2023-03-05 11:08:25 +0900

    add : notContainSpace to test

--- odp_calc_flutter_client/test/validator_test.dart
+++ odp_calc_flutter_client/test/validator_test.dart
@@ -3,7 +3,18 @@ import 'package:odp_calc_flutter_client/usecase/form/validator.dart';
 
 main() {
   group("validation", () {
-    test("半角・全角数字ではない", () {
+    test("半角・全角スペースを許容しない", () {
+      final val = Validator.notContainSpace('a　b');
+      expect(val, '空白は不要です');
+
+      final val2 = Validator.notContainSpace('a b');
+      expect(val2, '空白は不要です');
+
+      final val3 = Validator.notContainSpace('アムロジピン5mg');
+      expect(val3, null);
+    });
+
+    test("半角・全角数字のみの入力を許容しない", () {
       final val = Validator.isNotNum('１２３４５６７８９０');
       expect(val, '全角・半角数字のみは入力できません');
 
